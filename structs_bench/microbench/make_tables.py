27533c4daa2d98b0b89760cdffb203b761a0ffd3
demon1999 <drsanusha1@mail.ru>
2020-02-15 21:57:39 +0300

    fix

--- structs_bench/microbench/make_tables.py
+++ structs_bench/microbench/make_tables.py
@@ -1,26 +1,12 @@
+filenames = []
+num_of_runs = 5.0
 class Stats:
-    ops_real = 0.0
-    ops_cbtree = 0.0
-    ops_splaytree = 0.0
-    ops_updOpt1 = 0.0
-    ops_updOpt2 = 0.0
-    ops_updOpt5 = 0.0
-    ops_updOpt10 = 0.0
-    ops_updOpt30 = 0.0
-    ops_updOpt100 = 0.0
-    avgLen_real = 0.0
-    avgLen_cbtree = 0.0
-    avgLen_splaytree = 0.0
-    avgLen_updOpt1 = 0.0
-    avgLen_updOpt2 = 0.0
-    avgLen_updOpt5 = 0.0
-    avgLen_updOpt10 = 0.0
-    avgLen_updOpt30 = 0.0
-    avgLen_updOpt100 = 0.0
+    ops = {filename : 0.0 for filename in filenames}
+    avgLen = {filename : 0.0 for filename in filenames}
 
 stats = {"90/10" : Stats(), "95/5" : Stats(), "99/1" : Stats()}    
-#loop through all files
-for filename in ["skip_without_warmap", "flex_without_warmap", "cbtree_without_warmap"]:
+loop through all files
+for filename in filenames:
     inp = open("./results_for_table_2/" + filename, "r")
     # parameters:
     # threads: 1
@@ -47,59 +33,35 @@ for filename in ["skip_without_warmap", "flex_without_warmap", "cbtree_without_w
         upd = line.split(" ")
         results = lines[i + 9].split(", ")
         #print(x, y, secs)
-        if threads[1] == "1":
-            if secs[1] == "10":
-                ops = float((results[1].split(" "))[1]) / float(secs[1])
-                avgLen = float(results[2].split(" ")[1])
-                if (filename == "skip_without_wormap"):
-                    stats[x[1] + "/" + y[1]].ops_real += ops
-                    stats[x[1] + "/" + y[1]].avgLen_real += avgLen
-                if (filename == "cbtree_without_wormap"):
-                    stats[x[1] + "/" + y[1]].ops_cbtree += ops
-                    stats[x[1] + "/" + y[1]].avgLen_cbtree += avgLen    
-                if (filename == "flex_without_wormap"):
-                    if upd[2] == "1":
-                        stats[x[1] + "/" + y[1]].ops_updOpt1 += ops
-                        stats[x[1] + "/" + y[1]].avgLen_updOpt1 += avgLen
-                    if upd[2] == "2":
-                        stats[x[1] + "/" + y[1]].ops_updOpt2 += ops
-                        stats[x[1] + "/" + y[1]].avgLen_updOpt2 += avgLen
-                    if upd[2] == "5":
-                        stats[x[1] + "/" + y[1]].ops_updOpt5 += ops
-                        stats[x[1] + "/" + y[1]].avgLen_updOpt5 += avgLen
-                    if upd[2] == "10":
-                        stats[x[1] + "/" + y[1]].ops_updOpt10 += ops
-                        stats[x[1] + "/" + y[1]].avgLen_updOpt10 += avgLen    
-                    if upd[2] == "30":
-                        stats[x[1] + "/" + y[1]].ops_updOpt30 += ops
-                        stats[x[1] + "/" + y[1]].avgLen_updOpt30 += avgLen
-                    if upd[2] == "100":
-                        stats[x[1] + "/" + y[1]].ops_updOpt100 += ops
-                        stats[x[1] + "/" + y[1]].avgLen_updOpt100 += avgLen            
+        ops = float((results[1].split(" "))[1]) / float(secs[1])
+        avgLen = float(results[2].split(" ")[1])
+        stats[x[1] + "/" + y[1]].ops[filename] += ops;
+        stats[x[1] + "/" + y[1]].avgLen[filename] += ops;
     inp.close()
 out = open("./stats_tables.tex", "w")
-num_of_runs = 5.0
+
+
+
 for key in stats:
-    stats[key].ops_real /= num_of_runs
-    stats[key].ops_cbtree /= num_of_runs
-    stats[key].ops_updOpt1 /= num_of_runs
-    stats[key].ops_updOpt2 /= num_of_runs
-    stats[key].ops_updOpt5 /= num_of_runs
-    stats[key].ops_updOpt10 /= num_of_runs
-    stats[key].ops_updOpt30 /= num_of_runs
-    stats[key].ops_updOpt100 /= num_of_runs
-    stats[key].avgLen_real /= num_of_runs
-    stats[key].avgLen_cbtree /= num_of_runs
-    stats[key].avgLen_updOpt1 /= num_of_runs
-    stats[key].avgLen_updOpt2 /= num_of_runs
-    stats[key].avgLen_updOpt5 /= num_of_runs
-    stats[key].avgLen_updOpt10 /= num_of_runs
-    stats[key].avgLen_updOpt30 /= num_of_runs
-    stats[key].avgLen_updOpt100 /= num_of_runs
-    out.write("\\begin{tabular}{|c|c|c|c|c|c|c|c|c|}\n")
-    out.write("$"+ key + "$ & $skiplist$ & $cbtree$ & $p=\\frac{1}{1}$ & $p=\\frac{1}{2}$ & $p=\\frac{1}{5}$ & $p=\\frac{1}{10}$ & $p=\\frac{1}{30}$ & $p=\\frac{1}{100}$\\\\\\hline\n")
-    out.write("$ops/secs$ & {:.2f} & {:.2f} & {:.2f} & {:.2f} & {:.2f} & {:.2f} & {:.2f} & {:.2f}\\\\\\hline\n".format(stats[key].ops_real, stats[key].ops_cbtree, stats[key].ops_updOpt1, stats[key].ops_updOpt2, stats[key].ops_updOpt5, stats[key].ops_updOpt10, stats[key].ops_updOpt30, stats[key].ops_updOpt100))
-    out.write("$avgLen$ & {:.2f} & {:.2f} & {:.2f} & {:.2f} & {:.2f} & {:.2f} & {:.2f} & {:.2f}\\\\\\hline\n".format(stats[key].avgLen_real, stats[key].avgLen_cbtree, stats[key].avgLen_updOpt1, stats[key].avgLen_updOpt2, stats[key].avgLen_updOpt5, stats[key].avgLen_updOpt10, stats[key].avgLen_updOpt30, stats[key].avgLen_updOpt100))
+    for filename in filenames:
+        stats[key].ops[filename] /= num_of_runs
+        stats[key].avgLen[filename] /= num_of_runs
+    s = "\\begin{tabular}{|"
+    for i in range(len(filenames) + 1):
+        s += "c|"
+    s += "}\n"
+    out.write(s)
+    s = "$" + key + "$";
+    for filename in filenames:
+        s += " & $" + filename + "$"
+    out.write(s + "\\\\\\hline\n")  
+    s = "$ops/secs$"
+    t = "$avgLen"
+    for filename in filenames:
+        s += " & {:.2f}".format(stats[key].ops[filename])
+        t += " & {:.2f}".format(stats[key].avgLen[filename])
+    out.write(s + "\\\\\\hline\n")  
+    out.write(t + "\\\\\\hline\n")      
     out.write("\\end{tabular}\n")
     out.write("\\\\\\\\\n")
 out.close()
